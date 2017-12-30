ffa62a40ea358e218f4f2a794a5c3c9583dc1146
Robin Minervini <robin.minervini@gmail.com>
2017-12-30 13:20:09 +0100

    Passage du TableViewController de "All events" à un ViewController

--- FNFA/allEventsController.swift
+++ FNFA/allEventsController.swift
@@ -2,16 +2,18 @@
 //  allEventsController.swift
 //  FNFA
 //
-//  Created by Robin Minervini on 25/12/2017.
+//  Created by Robin Minervini on 30/12/2017.
 //  Copyright © 2017 Robin Minervini. All rights reserved.
 //
 
 import UIKit
 
-class allEventsController: UITableViewController {
+class allEventsController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var events = [EventsList]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,49 +24,14 @@ class allEventsController: UITableViewController {
         tableView.delegate = self
         tableView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-
-    // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return events.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
-        cell.textLabel?.text = events[indexPath.row].name
-        return cell
-    }
-    
-    // MARK: - Filter
-    
-    @IBAction func buttonAction(_ sender: Any) {
-        events = events.filter { $0.category == "Séance spéciale" }
-        self.tableView.reloadData()
-    }
-
-    @IBAction func filter2(_ sender: Any) {
-        events = events.filter { $0.category == "Salon des nouvelles écritures" }
-        self.tableView.reloadData()
-    }
-    
-    // MARK: - Pass data when tap on cell
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "showDetails", sender: self)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let destination = segue.destination as? SingleEventViewController {
-            destination.event = events[(tableView.indexPathForSelectedRow?.row)!]
-        }
+        // Dispose of any resources that can be recreated.
     }
     
     // MARK: - Load Json
-
+    
     func downloadJson(completed: @escaping () -> ()) {
         let path = Bundle.main.path(forResource: "events", ofType: "json")
         
@@ -82,14 +49,54 @@ class allEventsController: UITableViewController {
         }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - Table view data source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = events[indexPath.row].name
+        return cell
+    }
+    
+    // MARK: - Filter
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        events = events.filter { $0.category == "Séance spéciale" }
+        self.tableView.reloadData()
+    }
+    
+    @IBAction func filter2(_ sender: Any) {
+        events = events.filter { $0.category == "Salon des nouvelles écritures" }
+        self.tableView.reloadData()
+    }
+    
+    // MARK: - Pass data when tap on cell
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showDetails", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? SingleEventViewController {
+            destination.event = events[(tableView.indexPathForSelectedRow?.row)!]
+        }
+    }
+    
+    
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
